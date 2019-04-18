a028ad26f4d2b7e1d5ae6c6c05e5a22427f023f7
Daniel McClean <dmcclean01@hotmail.com>
2019-04-18 17:35:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var number 10
 
 function add5() {
@@ -26,3 +27,20 @@ console.log(number) // 15
 divideBy3()
 
 console.log(number) // 5
+=======
+function add(a, b) {
+  return 1 + 80;
+} // 81
+
+function subtract() {
+  expect(subtract(a, b)).toEqual(60 - 40);
+} // 20
+
+function multiply() {
+  expect(multiply(a, b)).toEqual(2 * 30);
+} // 6.8 (there's that floating-point arithmetic again...)
+
+function divide() {
+  expect(divide(a, b)).toEqual(5.0 / 2.5);
+} // 2
+>>>>>>> c46f0592cdbc51490df78c2256b7c10ee14389f7
